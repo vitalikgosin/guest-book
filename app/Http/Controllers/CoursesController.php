f4166c1670ed0bf510adfa5ce28848311f595e6b
Ilia Kondrashov <caseycs@gmail.com>
2019-06-20 20:46:35 +0200

    auto-apply ide plugin

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -9,7 +9,7 @@ class CoursesController extends Controller
     public function index(): \Illuminate\View\View
     {
         $courses = courses::where('published', 1)
-            ->orderBy('created_at')
+            ->orderByDesc('created_at')
             ->with('user')
             ->get();
         return view('courses', ['courses' => $courses]);
